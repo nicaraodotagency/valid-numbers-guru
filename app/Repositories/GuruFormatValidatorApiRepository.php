72b8aa41128593b588d0355629394663c013dd85
Harold UT <hautnep@gmail.com>
2021-10-05 16:11:47 -0600

    Feature #1 Add Repository Interface to consume Guru Format Validator Api

---
+++ app/Repositories/GuruFormatValidatorApiRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Contracts\GuruFormatValidatorApiRepositoryInterface;
+use Illuminate\Support\Facades\Http;
+
+class GuruFormatValidatorApiRepository implements GuruFormatValidatorApiRepositoryInterface
+{
+    protected $guruFormatValidatorApiClient;
+    protected $baseUrl;
+
+    public function __construct()
+    {
+        $this->guruFormatValidatorApiClient = Http::withToken(config('services.guru_format_validator_api.key'));
+        $this->baseUrl = config('services.guru_format_validator_api.url');
+    }
+
+    public function getAllLists(): array
+    {
+        $lists = $this->guruFormatValidatorApiClient->get("{$this->baseUrl}/api/lists");
+
+        return $lists->json();
+    }
+}
