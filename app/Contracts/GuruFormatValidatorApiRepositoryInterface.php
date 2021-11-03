c7197c1d047c1611f21235c76f6677c1399cb84e
Harold Ulloa Torres <hautnep@gmail.com>
2021-11-03 16:46:22 -0600

    feat: #14 Create Valid Format Api User when Guru User is created

--- app/Contracts/GuruFormatValidatorApiRepositoryInterface.php
+++ app/Contracts/GuruFormatValidatorApiRepositoryInterface.php
@@ -2,8 +2,12 @@
 
 namespace App\Contracts;
 
+use App\Models\User;
+
 interface GuruFormatValidatorApiRepositoryInterface
 {
     public function getAllLists(): array;
-    public function postList(array $numbers): array;
+    public function postList(User $user, array $numbers): array;
+    public function postUser(User $user): array;
+    function getUserToken(User $user): string;
 }
--- app/Repositories/GuruFormatValidatorApiRepository.php
+++ app/Repositories/GuruFormatValidatorApiRepository.php
@@ -3,17 +3,20 @@
 namespace App\Repositories;
 
 use App\Contracts\GuruFormatValidatorApiRepositoryInterface;
+use App\Models\User;
 use Illuminate\Support\Facades\Http;
 
 class GuruFormatValidatorApiRepository implements GuruFormatValidatorApiRepositoryInterface
 {
     protected $guruFormatValidatorApiClient;
     protected $baseUrl;
+    protected $baseWebhook;
 
     public function __construct()
     {
         $this->guruFormatValidatorApiClient = Http::withToken(config('services.guru_format_validator_api.key'));
         $this->baseUrl = config('services.guru_format_validator_api.url');
+        $this->baseWebhook = config('services.guru_format_validator_api.webhook');
     }
 
     public function getAllLists(): array
@@ -23,10 +26,31 @@ class GuruFormatValidatorApiRepository implements GuruFormatValidatorApiReposito
         return $lists->json();
     }
 
-    public function postList(array $numbers): array
+    public function postList(User $user, array $numbers): array
     {
-        $lists = $this->guruFormatValidatorApiClient->post("{$this->baseUrl}/api/lists", ["phoneNumbers" => $numbers]);
+        $lists = Http::withToken($this->getUserToken($user))->post("{$this->baseUrl}/api/lists", ["phoneNumbers" => $numbers]);
 
         return $lists->json();
+    }
+
+    public function postUser(User $user): array
+    {
+        $user = $this->guruFormatValidatorApiClient->post("{$this->baseUrl}/api/users", ["name" => $user->name, "email" => $user->email, "password" => $user->password, "role" => 'USER_ROLE']);
+
+        return $user->json();
+    }
+
+    function getUserToken(User $user): string {
+        if(is_null($user->format_validator_user_token)){
+            $guruFormatValidatorUser = $this->guruFormatValidatorApiClient->post("{$this->baseUrl}/api/auth/login", ["email" => $user->email, "password" => $user->password]);
+            $user->format_validator_user_token = ($guruFormatValidatorUser->json())["body"]["token"];
+            $user->save();
+        }
+        if(is_null($user->format_validator_user_webhook)) {
+            $guruFormatValidatorWebhook = Http::withToken($user->format_validator_user_token)->put("{$this->baseUrl}/api/webhooks", ["webhook" => $this->baseWebhook]);
+            $user->format_validator_user_webhook = ($guruFormatValidatorWebhook->json())["msg"]["body"];
+            $user->save();
+        }
+        return $user->format_validator_user_token;
     }
 }
