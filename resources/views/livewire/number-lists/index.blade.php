4b6cca090297a4643430b9e2157cea5256a88423
Harold Ulloa Torres <hautnep@gmail.com>
2021-11-04 15:50:59 -0600

    feat: Tweaks on Dashboard, icon, app name, footer and responsive tables.

--- resources/views/livewire/number-lists/index.blade.php
+++ resources/views/livewire/number-lists/index.blade.php
@@ -7,13 +7,13 @@
 
     @include('livewire.number-lists.create')
         <div class="rounded-lg block w-full overflow-x-auto p-6">
-            <table class="table table-bordered w-full mt-5 bg-gray-50 border border-gray-300">
+            <table class="table table-bordered w-full mt-5 bg-gray-50 border border-gray-300" style="min-width: 600px">
                 <thead>
                 <tr class="text-center border border-gray-300 p-2">
                     <th>No.</th>
                     <th>User Id</th>
                     <th>Created At</th>
-                    <th>Action</th>
+                    <th style="min-width: 200px">Action</th>
                 </tr>
                 </thead>
                 <tbody>
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-jet-application-mark class="block h-9 w-auto" />
+                        <img src="{{ url('Nicarao-Agency-Vertical-Logo-Full-Color-Square-Icon.png') }}" class="block h-9 w-auto">
                     </a>
                 </div>
 
